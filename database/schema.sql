ea7c3c9131bd7b740dd4385c7fb0dee1d1cf5a28
mingziV5 <ming_v5@163.com>
2017-04-26 01:38:55 +0800

    schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,19 @@
+Create Table: CREATE TABLE `idc` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `contacts` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8
+
+Create Table: CREATE TABLE `servers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(16) DEFAULT NULL,
+  `hostname` varchar(20) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `idc_id` int(11) DEFAULT NULL,
+  `remarks` varchar(100) DEFAULT NULL,
+  `create_time` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
